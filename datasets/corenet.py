85d809f0dc9fc5afb43dd25642d680a409a69fa5
Francis Engelmann <francis.engelmann@gmail.com>
2022-01-06 23:16:07 +0100

    Finalize corenet preprocessing sdf and point samples.

--- datasets/corenet.py
+++ datasets/corenet.py
@@ -1,2 +1,20 @@
-dataset_path = '/home/fengelmann/datasets/corenet/data'
+import os
+import json
 
+dataset_dir = '/home/fengelmann/datasets/corenet/data'
+
+for dataset in ['triplets', 'pairs']:
+  for split in ['test', 'val', 'train']:
+    dataset_path = os.path.join(dataset_dir, f'{dataset}.{split}')
+    dataset_json_file = os.path.join(dataset_path, 'dataset.json')
+    with open(dataset_json_file, 'r') as f:
+      json_file = json.load(f)
+      classes = json_file['classes']
+      classes_names = [c['human_readable'] for c in classes]
+      classes_ids = [c['id'] for c in classes]
+      print(split, dataset, classes_names, classes_ids)
+
+for i in range(len(classes_names)):
+  class_name = classes_names[i]
+  class_id = classes_ids[i]
+  print('| ' + class_id + ' | ' + class_name + ' \t |')
