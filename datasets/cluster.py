440dd390f71f65caedbfd6f044c53c20114c366d
Francis Engelmann <francis.engelmann@gmail.com>
2022-01-08 22:10:07 +0100

    Finish clustering

--- datasets/cluster.py
+++ datasets/cluster.py
@@ -1,45 +1,58 @@
 import os
 import pickle
+import math
 import numpy as np
 from absl import app
 from absl import flags
 from sklearn.cluster import KMeans
+import pyviz3d.visualizer as viz
 import utils
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string('sdf_path', '~/datasets/ShapeNetCore.v2.points_sdf', 'Prefix of path.')
+flags.DEFINE_string('shapenet_path', '~/datasets/ShapeNetCore.v2', 'Prefix of path.')
 flags.DEFINE_string('corenet_path', '~/datasets/corenet/data', 'Path to CoReNet dataset.')
 flags.DEFINE_integer('k', 50, 'Number of clusters per model class')
+flags.DEFINE_boolean('visualize', False, 'Visualize the cluster centers and the associated models.')
 
 
-def main(_):
-    # Get the name of the models in the training set
-    training_models_path_debug = 'training_models.pkl'
-    if os.path.exists(training_models_path_debug):
-        train_models = utils.load_pkl(training_models_path_debug)
-    else:
-        train_models = sorted(utils.corenet_models(FLAGS.corenet_path, splits=['train'], datasets=['triplets', 'pairs']))
-        utils.save_pkl(train_models, 'training_models.pkl')
-
-    # Load the SDFs of the training models
+def load_sdf(models: dict) -> dict:
     sdf_from_name_from_class = {}
-    for (model_class, mesh_name) in train_models:
+    for (model_class, mesh_name) in models:
         if model_class not in sdf_from_name_from_class:
             sdf_from_name_from_class[model_class] = {}
         sdf_filepath = os.path.join(FLAGS.sdf_path, model_class, mesh_name, 'models/model_normalized_sdf.npy')
         sdf_from_name_from_class[model_class][mesh_name] = np.load(os.path.expanduser(sdf_filepath))
+    return sdf_from_name_from_class
 
-    print('Number of models per class:')
-    for k in sdf_from_name_from_class.keys():
-        print(k, len(sdf_from_name_from_class[k]))
 
-    dict_class_model_cluster_id = {}
-    dict_cluster_center_class_nearest_model = {}
+def get_model_names(splits, pickle_file):
+    if os.path.exists(pickle_file):
+        model_names = utils.load_pkl(pickle_file)
+    else:
+        model_names = sorted(utils.corenet_models(FLAGS.corenet_path, splits=splits, datasets=['triplets', 'pairs']))
+        utils.save_pkl(model_names, pickle_file)
+    return model_names
 
+
+def cluster():
+    # Get the name of the models in the training set.
+    train_models = get_model_names(['train'], 'training_models.pkl')
+    train_val_test_models = get_model_names(['train', 'val', 'test'], 'train_val_test_models.pkl')
+
+    # Load the SDFs of the training models.
+    train_sdf_from_name_from_class = load_sdf(train_models)
+    train_val_test_sdf_from_name_from_class = load_sdf(train_val_test_models)
+
+    model_classes = train_sdf_from_name_from_class.keys()
+    cluster_center_id_from_name_from_class = {}
+    cluster_center_nearest_model_from_cluster_id = {}
     kmeans = KMeans(init='k-means++', n_clusters=FLAGS.k)
-    for i, class_id in enumerate(sdf_from_name_from_class.keys()):
+
+    # Cluster the training data and get cluster centers.
+    for i, class_id in enumerate(model_classes):
         print(f'Cluster SDFs of class {class_id}...')
-        sdf_from_name = sdf_from_name_from_class[class_id]
+        sdf_from_name = train_sdf_from_name_from_class[class_id]
         model_data = np.concatenate([np.reshape(v, [1, -1]) for k, v in sdf_from_name.items()], axis=0)
         model_names = [k for k in sdf_from_name.keys()]
         kmeans.fit(model_data)
@@ -51,20 +64,61 @@ def main(_):
             cluster_id = j + (i * FLAGS.k)
             cluster_center = kmeans.cluster_centers_[j]
             nearest_model_name = model_names[nearest_model_id_per_cluster[j]]
-            dict_cluster_center_class_nearest_model[cluster_id] = (cluster_center, class_id, nearest_model_name)
+            cluster_center_nearest_model_from_cluster_id[cluster_id] = (cluster_center, class_id, nearest_model_name)
+    utils.save_pkl(cluster_center_nearest_model_from_cluster_id, 'dict_clusterCenter_class_nearestModel.pkl')
 
+    # Predict cluster assignments of all models over all splits.
+    for i, class_id in enumerate(model_classes):
+        print('Assign cluster to the models over all splits...')
+        sdf_from_name = train_val_test_sdf_from_name_from_class[class_id]
+        model_data = np.concatenate([np.reshape(v, [1, -1]) for k, v in sdf_from_name.items()], axis=0)
+        model_names = [k for k in sdf_from_name.keys()]
         labels = kmeans.predict(model_data)
-        dict_class_model_cluster_id[class_id] = {}
+        cluster_center_id_from_name_from_class[class_id] = {}
         for j in range(labels.shape[0]):
             model_name = model_names[j]
             cluster_id = labels[j] + (i * FLAGS.k)
-            dict_class_model_cluster_id[class_id][model_name] = cluster_id
+            cluster_center_id_from_name_from_class[class_id][model_name] = cluster_id
+    utils.save_pkl(cluster_center_id_from_name_from_class, 'dict_class_model_clusterId.pkl')
 
-    with open(os.path.join('dict_class_model_clusterId.pkl'), 'wb') as f:
-        pickle.dump(dict_class_model_cluster_id, f)
 
-    with open(os.path.join('dict_clusterCenter_class_nearestModel.pkl'), 'wb') as f:
-        pickle.dump(dict_cluster_center_class_nearest_model, f)
+def visualize():
+    cluster_center_id_from_name_from_class = utils.load_pkl('dict_class_model_clusterId.pkl')
+    cluster_center_nearest_model_from_cluster_id = utils.load_pkl('dict_clusterCenter_class_nearestModel.pkl')
+
+    model_class = '03001627'
+    cluster_id_from_name = cluster_center_id_from_name_from_class[model_class]
+
+    v = viz.Visualizer()
+    for cluster_id in list(set([v for k, v in cluster_id_from_name.items()])):
+        cluster_center_nearest_model = cluster_center_nearest_model_from_cluster_id[cluster_id]
+        cluster_center_nearest_mesh_name = cluster_center_nearest_model[2]
+        path_obj = os.path.join(os.path.expanduser(FLAGS.shapenet_path), model_class,
+                                cluster_center_nearest_mesh_name, 'models/model_normalized.obj')
+        v.add_mesh(f'Cluster_{cluster_id}', path=path_obj, rotation=[math.pi/2, 0.0, 0.0],
+                   translation=[float(cluster_id % 50)/2.0, 0.0, 0.0], color=(np.random.rand(3) * 255).tolist())
+
+    cluster_center_id_from_name = cluster_center_id_from_name_from_class[model_class]
+
+    cluster_positions = {}
+    for mesh_name, cluster_id in list(cluster_center_id_from_name.items())[0:50]:
+        if cluster_id not in cluster_positions:
+            cluster_positions[cluster_id] = 1
+        else:
+            cluster_positions[cluster_id] += 1
+        path_obj = os.path.join(os.path.expanduser(FLAGS.shapenet_path), model_class,
+                                mesh_name, 'models/model_normalized.obj')
+        v.add_mesh(mesh_name, path=path_obj,
+                   rotation=[math.pi/2, 0.0, 0.0], translation=[float(cluster_id % 50)/2.0, cluster_positions[cluster_id], 0.0],
+                   color=(np.random.rand(3) * 255).tolist())
+    v.save('cluster_vis')
+
+
+def main(_):
+    if FLAGS.visualize:
+        visualize()
+    else:
+        cluster()
 
 
 if __name__ == '__main__':
